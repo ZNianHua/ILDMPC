671f8e7c8346345e72092587c64131a8e1a5da01
ZNianHua <96680190+ZNianHua@users.noreply.github.com>
2025-05-11 11:12:01 +0800

    Update ILMPCVehicleModel.m

--- ILMPCVehicleModel.m
+++ ILMPCVehicleModel.m
@@ -117,7 +117,7 @@ for i=1:Np
     else
         alpha_fl=-(delta_l-atan2((Vyp+a*theta_dotp),(Vxp-tfl*theta_dotp)) );
         alpha_fr=-(delta_r-atan2((Vyp+a*theta_dotp),(Vxp+tfr*theta_dotp)));
-        alpha_rl=(atan2((Vyp-b*theta_dotp),(Vxp+trl*theta_dotp)));
+        alpha_rl=(atan2((Vyp-b*theta_dotp),(Vxp-trl*theta_dotp)));
         alpha_rr=(atan2((Vyp-b*theta_dotp),(Vxp+trr*theta_dotp)));
     end
     
